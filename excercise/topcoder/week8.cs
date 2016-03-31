08f1dcdb130def4c94d3a4de5e5aebc2e1e9d456
snowyunee <snowyunee@gmail.com>
2016-03-31 11:18:03 +0900

    topcoder week8

--- excercise/topcoder/week8.cs
+++ excercise/topcoder/week8.cs
@@ -8,16 +8,78 @@ using System.Reactive.Linq;
 
 namespace topcoder
 {
+    public class GroupOfAdjacent<TSource, TKey> : IEnumerable<TSource>, IGrouping<TKey, TSource>
+    {
+        public TKey Key { get; set; }
+        private List<TSource> GroupList { get; set; }
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return ((System.Collections.Generic.IEnumerable<TSource>)this).GetEnumerator();
+        }
+        System.Collections.Generic.IEnumerator<TSource> System.Collections.Generic.IEnumerable<TSource>.GetEnumerator()
+        {
+            foreach (var s in GroupList)
+                yield return s;
+        }
+        public GroupOfAdjacent(List<TSource> source, TKey key)
+        {
+            GroupList = source;
+            Key = key;
+        }
+    }
+    public static class LocalExtensions
+    {
+        public static IEnumerable<IGrouping<TKey, TSource>> GroupAdjacent<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            TKey last = default(TKey);
+            bool haveLast = false;
+            List<TSource> list = new List<TSource>();
+            foreach (TSource s in source)
+            {
+                TKey k = keySelector(s);
+                if (haveLast)
+                {
+                    if (!k.Equals(last))
+                    {
+                        yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+                        list = new List<TSource>();
+                        list.Add(s);
+                        last = k;
+                    }
+                    else
+                    {
+                        list.Add(s);
+                        last = k;
+                    }
+                }
+                else
+                {
+                    list.Add(s);
+                    last = k;
+                    haveLast = true;
+                }
+            }
+            if (haveLast)
+                yield return new GroupOfAdjacent<TSource, TKey>(list, last);
+        }
+    }
     class week8
     {
+
         static int solve(int[] a)
         {
             try
             {
-              return a.GroupBy(x => x)
+              return a.GroupAdjacent(x =>
+                  {
+	                  //Console.WriteLine("x : {0}", x);
+	                  return x;
+	              })
                   .Select(xs =>
                   {
-//                      Console.WriteLine("{0} {1}", xs.Count(), xs.First());
+                      //Console.WriteLine("{0} {1}", xs.Count(), xs.First());
                       if (xs.Count() % xs.First() == 0)
                           return xs.Count() / xs.First();
                       throw new Exception();
