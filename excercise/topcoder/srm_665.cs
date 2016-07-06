a2ccd10bceaa04ac4cd98311a8523a751c5f3d99
snowyunee <snowyunee@gmail.com>
2016-07-06 18:42:52 +0900

    srm 665, 666

---
+++ excercise/topcoder/srm_665.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace topcoder
+{
+    // https://community.topcoder.com/stat?c=problem_statement&pm=13960
+    class srm_665
+    {
+        static int construct(int a)
+        {
+            try
+            {
+                return Enumerable.Range(a + 1, 100 - a)
+                    .SelectMany(x =>
+                    {
+                        var y = a ^ x;
+                        if (y == 4 || y == 7 || y == 44 || y == 47 ||
+                            y == 74 || y == 77)
+                            return Enumerable.Range(x, 1);
+                        return Enumerable.Range(x, 0);
+                    })
+                    .First();
+            }
+            catch (Exception /*e*/)
+            {
+                return -1;
+            }
+        }
+
+        static public void run()
+        {
+            //Console.WriteLine("Possible {0}", check(new String[] {"WWW"}));
+            Console.WriteLine("40 , {0}", construct(4));
+            Console.WriteLine("20 , {0}", construct(19));
+            Console.WriteLine("92 , {0}", construct(88));
+            Console.WriteLine("-1 , {0}", construct(36));
+            Console.WriteLine("-1 , {0}", construct(200));
+        }
+    }
+}
---
+++ excercise/topcoder/srm_666.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Disposables;
+
+namespace topcoder
+{
+    class srm_666
+    {
+        static String canWin(int[] nextLevel)
+        {
+            var l = Observable.Generate(
+                0,
+                s => nextLevel[s] != -1,
+                s => nextLevel[s],
+                s => nextLevel[s])
+                .Take(nextLevel.Count())
+                .Count()
+                .First();
+
+            Console.WriteLine(l);
+            if (l != nextLevel.Count()) return "Win";
+            return "Lose";
+        }
+        static public void run()
+        {
+            Console.WriteLine("{0}, {1}", "Win", canWin(new int[] { 1, -1 }));
+            Console.WriteLine("{0}, {1}", "Lose", canWin(new int[] { 1, 0, -1 }));
+            Console.WriteLine("{0}, {1}", "Lose", canWin(new int[] { 0, 1, 2 }));
+            Console.WriteLine("{0}, {1}", "Win", canWin(new int[] { 29, 33, 28, 16, -1, 11, 10, 14, 6, 31, 7, 35, 34, 8, 15, 17, 26, 12, 13, 22, 1, 20, 2, 21, -1, 5, 19, 9, 18, 4, 25, 32, 3, 30, 23, 10, 27 }));
+            Console.WriteLine("{0}, {1}", "Win", canWin(new int[] { 17, 43, 20, 41, 42, 15, 18, 35, -1, 31, 7, 33, 23, 33, -1, -1, 0, 33, 19, 12, 42, -1, -1, 9, 9, -1, 39, -1, 31, 46, -1, 20, 44, 41, -1, -1, 12, -1, 36, -1, -1, 6, 47, 10, 2, 4, 1, 29 }));
+            Console.WriteLine("{0}, {1}", "Lose", canWin(new int[] { 3, 1, 1, 2, -1, 4 }));
+        }
+    }
+}
